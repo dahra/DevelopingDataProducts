1c57081c7ced45e0089ff02ed2905a08193e69cd
dahra <kdahrajohnson@gmail.com>
2015-08-23 13:41:27 -0400

    project

--- server.R
+++ server.R
@@ -1,29 +1,34 @@
-#setwd("DevelopingDataProducts")
-# folder: census-app
-
 #server.R
 
+#setwd("C:/Users/dahra/Documents/R/win-library/3.2/UsingR/shiny")
+# folder: shiny
+
+library(devtools)
+library(shinyapps)
 library(shiny)
+library(htmlwidgets)
+#attach(arrests)
+
+arrests <- read.table("arrests-app/data/USArrests.txt", sep="\t",header = TRUE)
+
 attach(arrests)
 
-# Rely on the 'USArrests' dataset in the datasets
-# package (which generally comes preloaded).
+states <- levels(arrests$State)
 
-
-
-# Define a server for the Shiny app
+# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
-  data <- reactive({ 
-    c(mur<-m$mrank[m$State==input$states],
-    ast<-a$arank[a$State==input$states],
-    urb<-u$urank[u$State==input$states],
-    rap<-r$rrank[r$State==input$states]) 
-    
-  })
-   
-    # Fill in the spot we created for a plot
-    output$crimePlot <- renderPlot({
+  # Expression that generates a barplot. The expression is
+  # wrapped in a call to renderPlot to indicate that:
+  #
+  #  1) It is "reactive" and therefore should re-execute automatically
+  #     when inputs change
+  #  2) Its output type is a barplot of 4 Crime types per state
+  #  3) It also shows calculated crime rank of the state 
+  
+  
+  # Fill in the spot we created for a plot
+  output$crimePlot <- renderPlot({
     
     # Render a barplot
     b <- as.numeric(arrests[State==input$states,2:5])
@@ -36,44 +41,52 @@ shinyServer(function(input, output) {
             main=input$states, 
             xlim=c(0,max(arrests$Assault)),
             col=c("red","orange","blue","black")
-            )
+    )
     
-
+    
   })  #end of plot output
-
-    output$text1 <- renderText({ 
-      
-      m <- cbind(arrange(arrests,arrests[,2],State),mrank=c(1:nrow(arrests)))
-      mur<-m$mrank[m$State==input$states]
-      paste("Ranking Murder: ",mur)
-      
-    })
+  
+  ##TEXT OUTPUT
+  
+  output$text0 <- renderText({ 
     
+    paste("State Rank Out of 50 States (high ranks = high crime)")
     
-    output$text2 <- renderText({ 
-      
-      a <- cbind(arrange(arrests,arrests[,3],State),arank=c(1:nrow(arrests)))
-      ast<-a$arank[a$State==input$states]
-      paste("Ranking Assault: ",ast)
-      
-    })
+  })
+  
+  output$text1 <- renderText({ 
     
+    m <- cbind(arrange(arrests,arrests[,2],State),mrank=c(1:nrow(arrests)))
+    mur<-m$mrank[m$State==input$states]
+    paste("Ranking Murder: ",mur)
     
-    output$text3 <- renderText({ 
-      
-      u <- cbind(arrange(arrests,arrests[,4],State),urank=c(1:nrow(arrests)))
-      urb<-u$urank[u$State==input$states]
-      paste("Ranking Urban Pop: ",urb)
-      
-    })
+  })
+  
+  
+  output$text2 <- renderText({ 
     
+    a <- cbind(arrange(arrests,arrests[,3],State),arank=c(1:nrow(arrests)))
+    ast<-a$arank[a$State==input$states]
+    paste("Ranking Assault: ",ast)
     
-    output$text4 <- renderText({ 
-      
-      r <- cbind(arrange(arrests,arrests[,5],State),rrank=c(1:nrow(arrests)))
-      rap<-r$rrank[r$State==input$states]
-      paste("Ranking Rape: ",rap)
-      
-    })
+  })
+  
+  
+  output$text3 <- renderText({ 
     
+    u <- cbind(arrange(arrests,arrests[,4],State),urank=c(1:nrow(arrests)))
+    urb<-u$urank[u$State==input$states]
+    paste("Ranking Urban Pop: ",urb)
+    
+  })
+  
+  
+  output$text4 <- renderText({ 
+    
+    r <- cbind(arrange(arrests,arrests[,5],State),rrank=c(1:nrow(arrests)))
+    rap<-r$rrank[r$State==input$states]
+    paste("Ranking Rape: ",rap)
+    
+  })
+  
 }) #end of Shiny Server function
--- ui.R
+++ ui.R
@@ -1,54 +1,51 @@
-#setwd("C:/Users/dahra/Documents/R/WorkingDirectory/DevelopingDataProducts")
-setwd("C:/Users/dahra/Documents/R/win-library/3.2/UsingR/shiny")
-# folder: arrest-app
-
 #ui.R
 
+#setwd("C:/Users/dahra/Documents/R/win-library/3.2/UsingR/shiny")
+# folder: shiny
+
+library(devtools)
+library(shinyapps)
 library(shiny)
 library(plyr)
-# Rely on the 'USArrests' dataset in the datasets
-# package (which generally comes preloaded).
-#library(datasets)
+library(htmlwidgets)
 
-#setwd("DevelopingDataProducts")
+
 arrests <- read.table("arrests-app/data/USArrests.txt", sep="\t",header = TRUE)
+
+attach(arrests)
+
 states <- levels(arrests$State)
 
-# Define the overall UI
-shinyUI(
+
+# Define UI for application that draws a barplot
+# Use a fluid Bootstrap layout
+shinyUI(fluidPage(
   
-  # Use a fluid Bootstrap layout
-  fluidPage(    
+  # Application title
+  titlePanel("Crimes by State (1973)"),
+  
+  # Sidebar with a select input for US States
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("states", "Select a State:", 
+                  choices=states),
+      hr(),
+      helpText("Data from USArrests Dataset.")
+    ),
     
-    # Give the page a title
-    titlePanel("Crimes by State"),
-    
-    # Generate a row with a sidebar
-    sidebarLayout(      
-      
-      # Define the sidebar with one input
-      sidebarPanel(
-        selectInput("states", "State:", 
-                    choices=states),
-        hr(),
-        helpText("Data from USArrests Dataset.")
-      ),
-      
-      # Create a spot for the barplot
-      mainPanel(
-        plotOutput("crimePlot") 
-        ,
-        textOutput("text1")
-        ,
-        textOutput("text2")
-        ,
-        textOutput("text3")
-        ,
-        textOutput("text4")
-      )
-      
-
-    ) #end of sidebar layout (no comma)
-
-   )#end of fluidpage
-)
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("crimePlot") 
+      ,
+      textOutput("text0")
+      ,
+      textOutput("text1")
+      ,
+      textOutput("text2")
+      ,
+      textOutput("text3")
+      ,
+      textOutput("text4")
+    )
+  )
+))
